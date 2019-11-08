6a233e5ce9984a60f66bc4fd062a43fced45ff91
kozlyakovskaya-tatsiana <kozlyakovskaya_tatsiana@mail.ru>
2019-11-08 23:41:39 +0300

    Изменена реализация класса Sweet и Candy

--- NewYearGift/Sweets/Sweet.cs
+++ NewYearGift/Sweets/Sweet.cs
@@ -4,21 +4,46 @@ using System.Text;
 
 namespace NewYearGift.Sweets
 {
+    public enum Manufacturer
+    {
+        Spartak=1,
+        Kommunarka,
+        Sloduch,
+        Vitba
+    }
     public abstract class Sweet
     {
         public string Name { get; protected set; }
+
+        public Manufacturer Manufacturer { get; protected set; }
 
         public double WeightSingleItemInGrams { get; protected set; }
 
         public double GramsOfSugarSingleItem { get; protected set;}
 
-        protected Sweet (string name, double weightSingleItemInGrams, double gramsOfSugarSingleItem)
+        public Sweet()
+        {
+
+        }
+
+        protected void SetSweet(string name, Manufacturer manufacturer, double weightSingleItemInGrams, double gramsOfSugarSingleItem)
         {
             Name = name;
+            Manufacturer = manufacturer;
             WeightSingleItemInGrams = weightSingleItemInGrams;
             GramsOfSugarSingleItem = gramsOfSugarSingleItem;
         }
 
+       /* public Sweet(string name, Manufacturer manufacturer, double weightSingleItemInGrams, double gramsOfSugarSingleItem)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            WeightSingleItemInGrams = weightSingleItemInGrams;
+            GramsOfSugarSingleItem = gramsOfSugarSingleItem;
+        }*/
+
+       
+        
        /* public override string ToString()
         {
             return String.Format("{0,-9}{1,8:f2} grams {2,8:f2} grams of sugar",Name, WeightSingleItemInGrams, GramsOfSugarSingleItem);
--- NewYearGift/Sweets/SweetsInGift/Candy.cs
+++ NewYearGift/Sweets/SweetsInGift/Candy.cs
@@ -4,20 +4,65 @@ using System.Text;
 
 namespace NewYearGift.Sweets.SweetsInGift
 {
-    public enum TypeCandy
+   public enum KindCandy
     {
-        Chocolate=1,
+        Alenka=1,
+        Snickers,
+        Malibu,
+        Kapri,
+        BeeMaya
+    }
+
+    public enum FillingCandy
+    {
+        Chocolate = 1,
         Caramel,
         Jelly
     }
+
     sealed public class  Candy:Sweet
     {
-        public TypeCandy TypeCandy { get; private set; }
+        public KindCandy KindCandy { get; private set; }
 
-        public Candy(string name, double weightSingleItemInGrams, double gramsOfSugarSingleItem, TypeCandy typeCandy):
-            base(name, weightSingleItemInGrams, gramsOfSugarSingleItem)
+        public FillingCandy FillingCandy { get; private set; }
+
+        public Candy(KindCandy kindCandy)
         {
-            TypeCandy = typeCandy;
+            switch (kindCandy)
+            {
+                case KindCandy.Alenka:
+                    SetCandy("Alenka", Manufacturer.Kommunarka, 100, 50.3, KindCandy.Alenka, FillingCandy.Chocolate);
+                    break;
+                case KindCandy.BeeMaya:
+                    SetCandy("BeeMaya", Manufacturer.Spartak, 15, 4.5, KindCandy.BeeMaya, FillingCandy.Jelly);
+                    break;
+                case KindCandy.Kapri:
+                    SetCandy("Kapri", Manufacturer.Sloduch, 12, 4.3, KindCandy.Kapri, FillingCandy.Caramel);
+                    break;
+                case KindCandy.Malibu:
+                    SetCandy("Malibu", Manufacturer.Sloduch, 14, 5.7, KindCandy.Malibu, FillingCandy.Caramel);
+                    break;
+                case KindCandy.Snickers:
+                    SetCandy("Snickers", Manufacturer.Spartak, 85, 45.7, KindCandy.Snickers, FillingCandy.Chocolate);
+                    break;
+
+            }
         }
+
+        private void SetCandy(string name,
+            Manufacturer manufacturer,
+            double weightSingleItemInGrams,
+            double gramsOfSugarSingleItem,
+            KindCandy kindCandy,
+            FillingCandy fillingCandy)
+        {
+            base.SetSweet(name, manufacturer, weightSingleItemInGrams, gramsOfSugarSingleItem);
+            KindCandy = kindCandy;
+            FillingCandy = fillingCandy;
+        }
+
+
+
+
     }
 }
